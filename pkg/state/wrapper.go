96130a6f17209d3a600548c70b70255a9ac85d85
anorth <anorth@users.noreply.github.com>
2019-10-21 12:37:44 +1100

    Rework interfaces to provide a unified state wrapper, and state driver for manipulation/inspection

---
+++ pkg/state/wrapper.go
@@ -0,0 +1,33 @@
+package state
+
+import "github.com/ipfs/go-cid"
+
+// Wrapper abstracts the inspection and mutation of an implementation-specific state tree and storage.
+// The interface wraps a single, mutable state.
+type Wrapper interface {
+	// Returns the CID of the root node of the state tree.
+	Cid() cid.Cid
+	// Returns the actor state at `address` (or an error if there is none).
+	Actor(address Address) (Actor, error)
+	// Returns the actor storage for the actor at `address` (which is empty if there is no such actor).
+	Storage(address Address) (Storage, error)
+
+	// Creates a new private key and returns the associated address.
+	NewAccountAddress() (Address, error)
+	// Installs a new actor in the state tree.
+	// This signature will probably become a little more complex when the actor state is non-empty.
+	SetActor(address Address, code cid.Cid, balance AttoFIL) (Actor, Storage, error)
+}
+
+// Actor is an abstraction over the actor states stored in the root of the state tree.
+type Actor interface {
+	Code() cid.Cid
+	Head() cid.Cid
+	Nonce() uint64
+	Balance() AttoFIL
+}
+
+// Storage provides a key/value store for actor state.
+type Storage interface {
+	Get(cid cid.Cid) ([]byte, error)
+}
--- pkg/suites/example.go
+++ pkg/suites/example.go
@@ -13,51 +13,33 @@ import (
 
 // A basic example validation test.
 // At present this code is verbose and demonstrates the opportunity for helper methods.
-func Example(t *testing.T, driver Driver) {
-	actors := make(map[state.Address]state.Actor)
+func Example(t *testing.T, factories Factories) {
+	drv := NewStateDriver(t, factories.NewState())
 
-	alice, err := driver.NewAddress()
-	require.NoError(t, err)
-	actors[alice] = driver.NewActor(state.AccountActorCodeCid, big.NewInt(2000))
+	alice := drv.NewAccountActor(2000)
+	bob := drv.NewAccountActor(0)
+	miner := drv.NewAccountActor(0) // Miner owner
 
-	bob, err := driver.NewAddress()
-	require.NoError(t, err)
-	actors[bob] = driver.NewActor(state.AccountActorCodeCid, big.NewInt(0))
+	gasPrice := big.NewInt(1)
+	gasLimit := state.GasUnit(1000)
 
-	miner, err := driver.NewAddress()
-	require.NoError(t, err)
-	actors[miner] = driver.NewActor(state.AccountActorCodeCid, big.NewInt(0))
-
-	tree, storage, err := driver.NewState(actors)
-	require.NoError(t, err)
-	require.NotNil(t, tree)
-	require.NotNil(t, storage)
-
-	producer := chain.NewMessageProducer(driver, state.GasUnit(1000), big.NewInt(1))
+	producer := chain.NewMessageProducer(factories.NewMessageFactory(drv.State()), gasLimit, gasPrice)
 	msg, err := producer.Transfer(alice, bob, 0, 50)
-
-	exeCtx := chain.NewExecutionContext(1, miner)
-	validator := chain.NewValidator(driver)
-
-	endState, msgReceipt, err := validator.ApplyMessage(exeCtx, tree, storage, msg)
 	require.NoError(t, err)
-	require.NotNil(t, endState)
+
+	validator := chain.NewValidator(factories)
+	exeCtx := chain.NewExecutionContext(1, miner)
+
+	msgReceipt, err := validator.ApplyMessage(exeCtx, drv.State(), msg)
+	require.NoError(t, err)
 	require.NotNil(t, msgReceipt)
 
 	assert.Equal(t, uint8(0), msgReceipt.ExitCode)
 	assert.Equal(t, []byte{}, msgReceipt.ReturnValue)
 	assert.Equal(t, state.GasUnit(0), msgReceipt.GasUsed)
 
-	actorAlice, err := endState.Actor(alice)
-	require.NoError(t, err)
-	assert.Equal(t, state.AttoFIL(big.NewInt(1950)), actorAlice.Balance())
-
-	actorBob, err := endState.Actor(bob)
-	require.NoError(t, err)
-	assert.Equal(t, state.AttoFIL(big.NewInt(50)), actorBob.Balance())
-
+	drv.AssertBalance(alice, 1950)
+	drv.AssertBalance(bob, 50)
 	// This should become non-zero after gas tracking and payments are integrated.
-	actorMiner, err := endState.Actor(miner)
-	require.NoError(t, err)
-	assert.Equal(t, state.AttoFIL(big.NewInt(0)), actorMiner.Balance())
+	drv.AssertBalance(miner, 0)
 }
