85d48d1b052a685e9b1b24051bab4f31973e96e4
frrist <forrest@protocol.ai>
2019-12-02 13:11:24 -0800

    update serialization code

--- pkg/state/types/types.go
+++ pkg/state/types/types.go
@@ -1,12 +1,10 @@
 package types
 
 import (
-	"github.com/filecoin-project/go-leb128"
-	"github.com/ipfs/go-cid"
-	cbor "github.com/ipfs/go-ipld-cbor"
-	"github.com/pkg/errors"
+	"bytes"
+	"fmt"
 
-	"github.com/filecoin-project/chain-validation/pkg/state/address"
+	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
 // Type aliases for state values and message method parameters.
@@ -17,42 +15,18 @@ type (
 	PeerID string
 )
 
-// Given a slice of the above types encode them to CBOR byte array.
-func EncodeValues(params ...interface{}) ([]byte, error) {
-	if len(params) == 0 {
-		return []byte{}, nil
+func Serialize(i cbg.CBORMarshaler) ([]byte, error) {
+	buf := new(bytes.Buffer)
+	if err := i.MarshalCBOR(buf); err != nil {
+		return nil, err
 	}
-	var arr []interface{}
-	for i, p := range params {
-		bs, err := EncodeValue(p)
-		if err != nil {
-			return []byte{}, errors.Wrapf(err, "failed at index: %d", i)
-		}
-		arr = append(arr, bs)
-	}
-	return cbor.DumpObject(arr)
+	return buf.Bytes(), nil
 }
 
-// Given an above type encode it to CBOR.
-func EncodeValue(p interface{}) (interface{}, error) {
-	switch v := p.(type) {
-	case address.Address:
-		return v, nil
-	case BigInt:
-		return v, nil
-	case GasUnit:
-		return leb128.FromUInt64(uint64(v)), nil
-	case uint64:
-		return v, nil
-	case PubKey:
-		return v, nil
-	case PeerID:
-		return v, nil
-	case cid.Cid:
-		return v, nil
-	case []interface{}:
-		return EncodeValues(v...)
-	default:
-		return []byte{}, errors.Errorf("invalid type: %T", p)
+func Deserialize(b []byte, out interface{}) error {
+	um, ok := out.(cbg.CBORUnmarshaler)
+	if !ok {
+		return fmt.Errorf("type %T does not implement UnmarshalCBOR", out)
 	}
+	return um.UnmarshalCBOR(bytes.NewReader(b))
 }
