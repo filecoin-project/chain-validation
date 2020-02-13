076e4547d8bbd92cad7062f413e94b94bf131a0b
frrist <forrest@protocol.ai>
2020-02-13 15:13:46 -0800

    cleanup state access for multisig actor

--- chain/sugar.go
+++ chain/sugar.go
@@ -2,8 +2,10 @@ package chain
 
 import (
 	"github.com/filecoin-project/go-address"
+	abi_spec "github.com/filecoin-project/specs-actors/actors/abi"
 	builtin_spec "github.com/filecoin-project/specs-actors/actors/builtin"
 	init_spec "github.com/filecoin-project/specs-actors/actors/builtin/init"
+	multisig_spec "github.com/filecoin-project/specs-actors/actors/builtin/multisig"
 	paych_spec "github.com/filecoin-project/specs-actors/actors/builtin/paych"
 
 	"github.com/filecoin-project/chain-validation/chain/types"
@@ -16,12 +18,23 @@ func (mp *MessageProducer) Transfer(to, from address.Address, opts ...MsgOpt) *t
 	return mp.Build(to, from, builtin_spec.MethodSend, noParams, opts...)
 }
 
-func (mp *MessageProducer) CreatePaymentChannel(to, from address.Address, opts ...MsgOpt) *types.Message {
+func (mp *MessageProducer) CreatePaymentChannelActor(to, from address.Address, opts ...MsgOpt) *types.Message {
 	return mp.InitExec(builtin_spec.InitActorAddr, from, init_spec.ExecParams{
 		CodeCID: builtin_spec.PaymentChannelActorCodeID,
 		ConstructorParams: MustSerialize(&paych_spec.ConstructorParams{
 			From: from,
 			To:   to,
+		}),
+	}, opts...)
+}
+
+func (mp *MessageProducer) CreateMultisigActor(from address.Address, signers []address.Address, unlockDuration abi_spec.ChainEpoch, numApprovals int64, opts ...MsgOpt) *types.Message {
+	return mp.InitExec(builtin_spec.InitActorAddr, from, init_spec.ExecParams{
+		CodeCID: builtin_spec.MultisigActorCodeID,
+		ConstructorParams: MustSerialize(&multisig_spec.ConstructorParams{
+			Signers:               signers,
+			NumApprovalsThreshold: numApprovals,
+			UnlockDuration:        unlockDuration,
 		}),
 	}, opts...)
 }
--- drivers/test.go
+++ drivers/test.go
@@ -16,7 +16,6 @@ import (
 	abi_spec "github.com/filecoin-project/specs-actors/actors/abi"
 	big_spec "github.com/filecoin-project/specs-actors/actors/abi/big"
 	builtin_spec "github.com/filecoin-project/specs-actors/actors/builtin"
-	init_spec "github.com/filecoin-project/specs-actors/actors/builtin/init"
 	multisig_spec "github.com/filecoin-project/specs-actors/actors/builtin/multisig"
 	adt_spec "github.com/filecoin-project/specs-actors/actors/util/adt"
 
@@ -154,25 +153,14 @@ func (td *TestDriver) AssertBalance(addr address.Address, expected big_spec.Int)
 	assert.Equal(td.T, expected, actr.Balance(), fmt.Sprintf("expected balance: %v, actual balance: %v", expected, actr.Balance().String()))
 }
 
-// AssertReceipt checks that a receipt is not nill and has values equal to `expected`.
-func (td *TestDriver) AssertReceipt(receipt, expected types.MessageReceipt) {
-	assert.NotNil(td.T, receipt)
-	assert.Equal(td.T, expected.GasUsed, receipt.GasUsed, fmt.Sprintf("expected gas: %v, actual gas: %v", expected.GasUsed, receipt.GasUsed))
-	assert.Equal(td.T, expected.ReturnValue, receipt.ReturnValue, fmt.Sprintf("expected return value: %v, actual return value: %v", expected.ReturnValue, receipt.ReturnValue))
-	assert.Equal(td.T, expected.ExitCode, receipt.ExitCode, fmt.Sprintf("expected exit code: %v, actual exit code: %v", expected.ExitCode, receipt.ExitCode))
-}
-
 func (td *TestDriver) AssertMultisigTransaction(multisigAddr address.Address, txnID multisig_spec.TxnID, txn multisig_spec.Transaction) {
-	multisigActor, err := td.State().Actor(multisigAddr)
-	require.NoError(td.T, err)
+	var msState multisig_spec.State
+	td.GetActorState(multisigAddr, &msState)
 
 	strg, err := td.State().Storage()
 	require.NoError(td.T, err)
 
-	var multisig multisig_spec.State
-	require.NoError(td.T, strg.Get(context.Background(), multisigActor.Head(), &multisig))
-
-	txnMap := adt_spec.AsMap(strg, multisig.PendingTxns)
+	txnMap := adt_spec.AsMap(strg, msState.PendingTxns)
 	var actualTxn multisig_spec.Transaction
 	found, err := txnMap.Get(txnID, &actualTxn)
 	assert.NoError(td.T, err)
@@ -182,16 +170,13 @@ func (td *TestDriver) AssertMultisigTransaction(multisigAddr address.Address, tx
 }
 
 func (td *TestDriver) AssertMultisigContainsTransaction(multisigAddr address.Address, txnID multisig_spec.TxnID, contains bool) {
-	multisigActor, err := td.State().Actor(multisigAddr)
-	require.NoError(td.T, err)
+	var msState multisig_spec.State
+	td.GetActorState(multisigAddr, &msState)
 
 	strg, err := td.State().Storage()
 	require.NoError(td.T, err)
 
-	var multisig multisig_spec.State
-	require.NoError(td.T, strg.Get(context.Background(), multisigActor.Head(), &multisig))
-
-	txnMap := adt_spec.AsMap(strg, multisig.PendingTxns)
+	txnMap := adt_spec.AsMap(strg, msState.PendingTxns)
 	var actualTxn multisig_spec.Transaction
 	found, err := txnMap.Get(txnID, &actualTxn)
 	require.NoError(td.T, err)
@@ -200,43 +185,26 @@ func (td *TestDriver) AssertMultisigContainsTransaction(multisigAddr address.Add
 }
 
 func (td *TestDriver) AssertMultisigState(multisigAddr address.Address, expected multisig_spec.State) {
-	multisigActor, err := td.State().Actor(multisigAddr)
-	require.NoError(td.T, err)
-
-	strg, err := td.State().Storage()
-	require.NoError(td.T, err)
-
-	var multisig multisig_spec.State
-	require.NoError(td.T, strg.Get(context.Background(), multisigActor.Head(), &multisig))
-
-	assert.NotNil(td.T, multisig)
-	assert.Equal(td.T, expected.InitialBalance, multisig.InitialBalance, fmt.Sprintf("expected InitialBalance: %v, actual InitialBalance: %v", expected.InitialBalance, multisig.InitialBalance))
-	assert.Equal(td.T, expected.NextTxnID, multisig.NextTxnID, fmt.Sprintf("expected NextTxnID: %v, actual NextTxnID: %v", expected.NextTxnID, multisig.NextTxnID))
-	assert.Equal(td.T, expected.NumApprovalsThreshold, multisig.NumApprovalsThreshold, fmt.Sprintf("expected NumApprovalsThreshold: %v, actual NumApprovalsThreshold: %v", expected.NumApprovalsThreshold, multisig.NumApprovalsThreshold))
-	assert.Equal(td.T, expected.StartEpoch, multisig.StartEpoch, fmt.Sprintf("expected StartEpoch: %v, actual StartEpoch: %v", expected.StartEpoch, multisig.StartEpoch))
-	assert.Equal(td.T, expected.UnlockDuration, multisig.UnlockDuration, fmt.Sprintf("expected UnlockDuration: %v, actual UnlockDuration: %v", expected.UnlockDuration, multisig.UnlockDuration))
+	var msState multisig_spec.State
+	td.GetActorState(multisigAddr, &msState)
+	assert.NotNil(td.T, msState)
+	assert.Equal(td.T, expected.InitialBalance, msState.InitialBalance, fmt.Sprintf("expected InitialBalance: %v, actual InitialBalance: %v", expected.InitialBalance, msState.InitialBalance))
+	assert.Equal(td.T, expected.NextTxnID, msState.NextTxnID, fmt.Sprintf("expected NextTxnID: %v, actual NextTxnID: %v", expected.NextTxnID, msState.NextTxnID))
+	assert.Equal(td.T, expected.NumApprovalsThreshold, msState.NumApprovalsThreshold, fmt.Sprintf("expected NumApprovalsThreshold: %v, actual NumApprovalsThreshold: %v", expected.NumApprovalsThreshold, msState.NumApprovalsThreshold))
+	assert.Equal(td.T, expected.StartEpoch, msState.StartEpoch, fmt.Sprintf("expected StartEpoch: %v, actual StartEpoch: %v", expected.StartEpoch, msState.StartEpoch))
+	assert.Equal(td.T, expected.UnlockDuration, msState.UnlockDuration, fmt.Sprintf("expected UnlockDuration: %v, actual UnlockDuration: %v", expected.UnlockDuration, msState.UnlockDuration))
 
 	for _, e := range expected.Signers {
-		assert.Contains(td.T, multisig.Signers, e, fmt.Sprintf("expected Signer: %v, actual Signer: %v", e, multisig.Signers))
+		assert.Contains(td.T, msState.Signers, e, fmt.Sprintf("expected Signer: %v, actual Signer: %v", e, msState.Signers))
 	}
 }
 
 func (td *TestDriver) MustCreateAndVerifyMultisigActor(nonce int64, value abi_spec.TokenAmount, multisigAddr address.Address, from address.Address, params *multisig_spec.ConstructorParams, receipt types.MessageReceipt) {
 	/* Create the Multisig actor*/
-	multiSigConstuctParams, err := chain.Serialize(params)
-	require.NoError(td.T, err)
-
-	msg := td.Producer.InitExec(builtin_spec.InitActorAddr, from, init_spec.ExecParams{
-		CodeCID:           builtin_spec.MultisigActorCodeID,
-		ConstructorParams: multiSigConstuctParams,
-	}, chain.Value(value), chain.Nonce(nonce))
-
-	msgReceipt, err := td.Validator.ApplyMessage(td.ExeCtx, td.State(), msg)
-	require.NoError(td.T, err)
-
-	/* Assert the message was applied successfully  */
-	td.AssertReceipt(msgReceipt, receipt)
-
+	td.ApplyMessageExpectReceipt(
+		td.Producer.CreateMultisigActor(from, params.Signers, params.UnlockDuration, params.NumApprovalsThreshold, chain.Nonce(nonce), chain.Value(value)),
+		receipt,
+	)
 	/* Assert the actor state was setup as expected */
 	pendingTxMap, err := adt_spec.MakeEmptyMap(newMockStore())
 	require.NoError(td.T, err)
@@ -252,32 +220,4 @@ func (td *TestDriver) MustCreateAndVerifyMultisigActor(nonce int64, value abi_sp
 		PendingTxns: pendingTxMap.Root(),
 	})
 	td.AssertBalance(multisigAddr, value)
-}
-
-func (td *TestDriver) MustProposeMultisigTransfer(nonce int64, value abi_spec.TokenAmount, txID multisig_spec.TxnID, multisigAddr, from address.Address, params multisig_spec.ProposeParams, receipt types.MessageReceipt) {
-	/* Propose the transactions */
-	msg := td.Producer.MultisigPropose(multisigAddr, from, params, chain.Value(value), chain.Nonce(nonce))
-	msgReceipt, err := td.Validator.ApplyMessage(td.ExeCtx, td.State(), msg)
-	require.NoError(td.T, err)
-
-	/* Assert it was applied successfully  */
-	td.AssertReceipt(msgReceipt, receipt)
-}
-
-func (td *TestDriver) MustApproveMultisigActor(nonce int64, value abi_spec.TokenAmount, ms, from address.Address, txID multisig_spec.TxnID, receipt types.MessageReceipt) {
-	msg := td.Producer.MultisigApprove(ms, from, multisig_spec.TxnIDParams{ID: txID}, chain.Value(value), chain.Nonce(nonce))
-
-	msgReceipt, err := td.Validator.ApplyMessage(td.ExeCtx, td.State(), msg)
-	require.NoError(td.T, err)
-
-	td.AssertReceipt(msgReceipt, receipt)
-}
-
-func (td *TestDriver) MustCancelMultisigActor(nonce int64, value abi_spec.TokenAmount, ms, from address.Address, txID multisig_spec.TxnID, receipt types.MessageReceipt) {
-	msg := td.Producer.MultisigCancel(ms, from, multisig_spec.TxnIDParams{ID: txID}, chain.Value(value), chain.Nonce(nonce))
-
-	msgReceipt, err := td.Validator.ApplyMessage(td.ExeCtx, td.State(), msg)
-	require.NoError(td.T, err)
-
-	td.AssertReceipt(msgReceipt, receipt)
 }
