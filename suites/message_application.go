75c9f64c3fc2056231bb23d4145ec0377f5dbe83
frrist <forrest@protocol.ai>
2020-02-24 15:49:05 -0800

    gofmt files

--- suites/message_application.go
+++ suites/message_application.go
@@ -62,95 +62,95 @@ func TestMessageApplicationEdgecases(t *testing.T, factory state.Factories) {
 			types.MessageReceipt{ExitCode: exitcode.SysErrOutOfGas, ReturnValue: EmptyReturnValue, GasUsed: gasCost},
 		)
 	})
-		t.Run("not enough gas to pay message on-chain-size cost", func(t *testing.T) {
-			td := builder.Build(t)
+	t.Run("not enough gas to pay message on-chain-size cost", func(t *testing.T) {
+		td := builder.Build(t)
 
-			var aliceBal = abi_spec.NewTokenAmount(1_000_000_000)
-			var transferAmnt = abi_spec.NewTokenAmount(10)
-			var gasCost = big_spec.Zero()
+		var aliceBal = abi_spec.NewTokenAmount(1_000_000_000)
+		var transferAmnt = abi_spec.NewTokenAmount(10)
+		var gasCost = big_spec.Zero()
 
-			alice := td.NewAccountActor(drivers.SECP, aliceBal)
+		alice := td.NewAccountActor(drivers.SECP, aliceBal)
 
-			// Expect Message application to fail due to lack of gas
-			td.ApplyMessageExpectReceipt(
-				td.Producer.Transfer(alice, alice, chain.Value(transferAmnt), chain.Nonce(0), chain.GasPrice(10), chain.GasLimit(1)),
-				types.MessageReceipt{ExitCode: exitcode.SysErrOutOfGas, ReturnValue: EmptyReturnValue, GasUsed: gasCost},
-			)
+		// Expect Message application to fail due to lack of gas
+		td.ApplyMessageExpectReceipt(
+			td.Producer.Transfer(alice, alice, chain.Value(transferAmnt), chain.Nonce(0), chain.GasPrice(10), chain.GasLimit(1)),
+			types.MessageReceipt{ExitCode: exitcode.SysErrOutOfGas, ReturnValue: EmptyReturnValue, GasUsed: gasCost},
+		)
 
-			// Expect Message application to fail due to lack of gas when sender address is unknown
-			unknown := utils.NewIDAddr(t, 10000000)
-			td.ApplyMessageExpectReceipt(
-				td.Producer.Transfer(alice, unknown, chain.Value(transferAmnt), chain.Nonce(0), chain.GasPrice(10), chain.GasLimit(1)),
-				types.MessageReceipt{ExitCode: exitcode.SysErrOutOfGas, ReturnValue: EmptyReturnValue, GasUsed: gasCost},
-			)
+		// Expect Message application to fail due to lack of gas when sender address is unknown
+		unknown := utils.NewIDAddr(t, 10000000)
+		td.ApplyMessageExpectReceipt(
+			td.Producer.Transfer(alice, unknown, chain.Value(transferAmnt), chain.Nonce(0), chain.GasPrice(10), chain.GasLimit(1)),
+			types.MessageReceipt{ExitCode: exitcode.SysErrOutOfGas, ReturnValue: EmptyReturnValue, GasUsed: gasCost},
+		)
 
-		})
+	})
 
-		t.Run("invalid actor CallSeqNum", func(t *testing.T) {
-			td := builder.Build(t)
+	t.Run("invalid actor CallSeqNum", func(t *testing.T) {
+		td := builder.Build(t)
 
-			var aliceBal = abi_spec.NewTokenAmount(1_000_000_000)
-			var transferAmnt = abi_spec.NewTokenAmount(10)
-			var gasCost = big_spec.Zero()
+		var aliceBal = abi_spec.NewTokenAmount(1_000_000_000)
+		var transferAmnt = abi_spec.NewTokenAmount(10)
+		var gasCost = big_spec.Zero()
 
-			alice := td.NewAccountActor(drivers.SECP, aliceBal)
+		alice := td.NewAccountActor(drivers.SECP, aliceBal)
 
-			// Expect Message application to fail due to callseqnum being invalid: 1 instead of 0
-			td.ApplyMessageExpectReceipt(
-				td.Producer.Transfer(alice, alice, chain.Value(transferAmnt), chain.Nonce(1)),
-				types.MessageReceipt{ExitCode: exitcode.SysErrInvalidCallSeqNum, ReturnValue: EmptyReturnValue, GasUsed: gasCost},
-			)
+		// Expect Message application to fail due to callseqnum being invalid: 1 instead of 0
+		td.ApplyMessageExpectReceipt(
+			td.Producer.Transfer(alice, alice, chain.Value(transferAmnt), chain.Nonce(1)),
+			types.MessageReceipt{ExitCode: exitcode.SysErrInvalidCallSeqNum, ReturnValue: EmptyReturnValue, GasUsed: gasCost},
+		)
 
-			// Expect message application to fail due to unknow actor when call seq num is also incorrect
-			unknown := utils.NewIDAddr(t, 10000000)
-			td.ApplyMessageExpectReceipt(
-				td.Producer.Transfer(alice, unknown, chain.Value(transferAmnt), chain.Nonce(1)),
-				types.MessageReceipt{ExitCode: exitcode.SysErrActorNotFound, ReturnValue: EmptyReturnValue, GasUsed: gasCost},
-			)
-		})
+		// Expect message application to fail due to unknow actor when call seq num is also incorrect
+		unknown := utils.NewIDAddr(t, 10000000)
+		td.ApplyMessageExpectReceipt(
+			td.Producer.Transfer(alice, unknown, chain.Value(transferAmnt), chain.Nonce(1)),
+			types.MessageReceipt{ExitCode: exitcode.SysErrActorNotFound, ReturnValue: EmptyReturnValue, GasUsed: gasCost},
+		)
+	})
 
-		t.Run("abort during actor execution", func(t *testing.T) {
-			td := builder.Build(t)
+	t.Run("abort during actor execution", func(t *testing.T) {
+		td := builder.Build(t)
 
-			const pcTimeLock = abi_spec.ChainEpoch(10)
-			const pcLane = uint64(123)
-			const pcNonce = uint64(1)
-			var pcAmount = big_spec.NewInt(10)
-			var initialBal = abi_spec.NewTokenAmount(200_000_000_000)
-			var toSend = abi_spec.NewTokenAmount(10_000)
-			var pcSig = &crypto_spec.Signature{
-				Type: crypto_spec.SigTypeBLS,
-				Data: []byte("Grrr im an invalid signature, I cause panics in the payment channel actor"),
-			}
+		const pcTimeLock = abi_spec.ChainEpoch(10)
+		const pcLane = uint64(123)
+		const pcNonce = uint64(1)
+		var pcAmount = big_spec.NewInt(10)
+		var initialBal = abi_spec.NewTokenAmount(200_000_000_000)
+		var toSend = abi_spec.NewTokenAmount(10_000)
+		var pcSig = &crypto_spec.Signature{
+			Type: crypto_spec.SigTypeBLS,
+			Data: []byte("Grrr im an invalid signature, I cause panics in the payment channel actor"),
+		}
 
-			// will create and send on payment channel
-			sender := td.NewAccountActor(drivers.SECP, initialBal) // 100
-			senderID := utils.NewIDAddr(t, 100)
+		// will create and send on payment channel
+		sender := td.NewAccountActor(drivers.SECP, initialBal) // 100
+		senderID := utils.NewIDAddr(t, 100)
 
-			// will be receiver on paych
-			receiver := td.NewAccountActor(drivers.SECP, initialBal) // 101
-			//receiverID := utils.NewIDAddr(t, 101)
+		// will be receiver on paych
+		receiver := td.NewAccountActor(drivers.SECP, initialBal) // 101
+		//receiverID := utils.NewIDAddr(t, 101)
 
-			// the _expected_ address of the payment channel
-			paychAddr := utils.NewIDAddr(t, 102) // 103
-			createRet := td.ComputeInitActorExecReturn(senderID, 0, paychAddr)
-			td.ApplyMessageExpectReceipt(
-				td.Producer.CreatePaymentChannelActor(receiver, sender, chain.Value(toSend), chain.Nonce(0)),
-				types.MessageReceipt{ExitCode: exitcode.Ok, ReturnValue: chain.MustSerialize(&createRet), GasUsed: big_spec.Zero()},
-			)
+		// the _expected_ address of the payment channel
+		paychAddr := utils.NewIDAddr(t, 102) // 103
+		createRet := td.ComputeInitActorExecReturn(senderID, 0, paychAddr)
+		td.ApplyMessageExpectReceipt(
+			td.Producer.CreatePaymentChannelActor(receiver, sender, chain.Value(toSend), chain.Nonce(0)),
+			types.MessageReceipt{ExitCode: exitcode.Ok, ReturnValue: chain.MustSerialize(&createRet), GasUsed: big_spec.Zero()},
+		)
 
-			// message application fails due to invalid argument (signature).
-			td.ApplyMessageExpectReceipt(
-				td.Producer.PaychUpdateChannelState(paychAddr, sender, paych_spec.UpdateChannelStateParams{
-					Sv: paych_spec.SignedVoucher{
-						TimeLock:  pcTimeLock,
-						Lane:      pcLane,
-						Nonce:     pcNonce,
-						Amount:    pcAmount,
-						Signature: pcSig, // construct with invalid signature
-					},
-				}, chain.Nonce(1), chain.Value(big_spec.Zero())),
-				types.MessageReceipt{ExitCode: exitcode.ErrIllegalArgument, ReturnValue: EmptyReturnValue, GasUsed: big_spec.Zero()},
-			)
-		})
+		// message application fails due to invalid argument (signature).
+		td.ApplyMessageExpectReceipt(
+			td.Producer.PaychUpdateChannelState(paychAddr, sender, paych_spec.UpdateChannelStateParams{
+				Sv: paych_spec.SignedVoucher{
+					TimeLock:  pcTimeLock,
+					Lane:      pcLane,
+					Nonce:     pcNonce,
+					Amount:    pcAmount,
+					Signature: pcSig, // construct with invalid signature
+				},
+			}, chain.Nonce(1), chain.Value(big_spec.Zero())),
+			types.MessageReceipt{ExitCode: exitcode.ErrIllegalArgument, ReturnValue: EmptyReturnValue, GasUsed: big_spec.Zero()},
+		)
+	})
 }
