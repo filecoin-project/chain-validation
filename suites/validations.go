419cc6874b221bd95134369a23b10f6485b55c6e
Frrist <forrest@protocol.ai>
2020-03-02 11:21:17 -0800

    return validation tests as a slice (#93)
    - implementations can update chain-validation and run all new tests
    without updating their test files

---
+++ suites/validations.go
@@ -0,0 +1,29 @@
+package suites
+
+import (
+	"testing"
+
+	"github.com/filecoin-project/chain-validation/state"
+	"github.com/filecoin-project/chain-validation/suites/message"
+	"github.com/filecoin-project/chain-validation/suites/tipset"
+)
+
+type TestCase func(t *testing.T, factory state.Factories)
+
+func MessageTestCases() []TestCase {
+	return []TestCase{
+		message.TestAccountActorCreation,
+		message.TestInitActorSequentialIDAddressCreate,
+		message.TestMessageApplicationEdgecases,
+		message.TestMultiSigActor,
+		message.TestPaych,
+		message.TestValueTransferAdvance,
+		message.TestValueTransferSimple,
+	}
+}
+
+func TipSetTestCases() []TestCase {
+	return []TestCase{
+		tipset.TestBlockMessageInfoApplication,
+	}
+}
