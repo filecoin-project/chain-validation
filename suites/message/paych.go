d509c0499c20d00712f22df6b3290dcef2b3c9be
Frrist <forrest@protocol.ai>
2020-03-02 17:10:33 -0800

    fix paych actor tests (#96)

--- suites/message/paych.go
+++ suites/message/paych.go
@@ -31,13 +31,13 @@ func TestPaych(t *testing.T, factory state.Factories) {
 		td := builder.Build(t)
 
 		// will create and send on payment channel
-		sender, senderID := td.NewAccountActor(drivers.SECP, initialBal) // 100
+		sender, senderID := td.NewAccountActor(drivers.SECP, initialBal)
 
 		// will be receiver on paych
-		receiver, receiverID := td.NewAccountActor(drivers.SECP, initialBal) // 101
+		receiver, receiverID := td.NewAccountActor(drivers.SECP, initialBal)
 
 		// the _expected_ address of the payment channel
-		paychAddr := utils.NewIDAddr(t, 102) // 102
+		paychAddr := utils.NewIDAddr(t, utils.IdFromAddress(receiverID)+1)
 		createRet := td.ComputeInitActorExecReturn(senderID, 0, paychAddr)
 
 		// init actor creates the payment channel
@@ -56,7 +56,7 @@ func TestPaych(t *testing.T, factory state.Factories) {
 	t.Run("happy path update", func(t *testing.T) {
 		td := builder.Build(t)
 
-		const pcTimeLock = abi_spec.ChainEpoch(10)
+		const pcTimeLock = abi_spec.ChainEpoch(1)
 		const pcLane = uint64(123)
 		const pcNonce = uint64(1)
 		var pcAmount = big_spec.NewInt(10)
@@ -66,13 +66,13 @@ func TestPaych(t *testing.T, factory state.Factories) {
 		}
 
 		// will create and send on payment channel
-		sender, senderID := td.NewAccountActor(drivers.SECP, initialBal) // 100
+		sender, senderID := td.NewAccountActor(drivers.SECP, initialBal)
 
 		// will be receiver on paych
-		receiver, _ := td.NewAccountActor(drivers.SECP, initialBal) // 101
+		receiver, receiverID := td.NewAccountActor(drivers.SECP, initialBal)
 
 		// the _expected_ address of the payment channel
-		paychAddr := utils.NewIDAddr(t, 102) // 102
+		paychAddr := utils.NewIDAddr(t, utils.IdFromAddress(receiverID)+1)
 		createRet := td.ComputeInitActorExecReturn(senderID, 0, paychAddr)
 		td.ApplyMessageExpectReceipt(
 			td.MessageProducer.CreatePaymentChannelActor(receiver, sender, chain.Value(toSend), chain.Nonce(0)),
@@ -83,7 +83,7 @@ func TestPaych(t *testing.T, factory state.Factories) {
 			td.MessageProducer.PaychUpdateChannelState(paychAddr, sender, paych_spec.UpdateChannelStateParams{
 				Sv: paych_spec.SignedVoucher{
 					TimeLockMin: pcTimeLock,
-					TimeLockMax: pcTimeLock,
+					TimeLockMax: 0, // TimeLockMax set to 0 means no timeout
 					Lane:        pcLane,
 					Nonce:       pcNonce,
 					Amount:      pcAmount,
@@ -105,21 +105,48 @@ func TestPaych(t *testing.T, factory state.Factories) {
 		td := builder.Build(t)
 
 		// create the payment channel
-		sender, _ := td.NewAccountActor(drivers.SECP, initialBal)   // 101
-		receiver, _ := td.NewAccountActor(drivers.SECP, initialBal) // 102
-		paychAddr := utils.NewIDAddr(t, 103)                        // 103
+		sender, senderID := td.NewAccountActor(drivers.SECP, initialBal)
+		receiver, receiverID := td.NewAccountActor(drivers.SECP, initialBal)
+		paychAddr := utils.NewIDAddr(t, utils.IdFromAddress(receiverID)+1)
+		initRet := td.ComputeInitActorExecReturn(senderID, 0, paychAddr)
 		td.ApplyMessageExpectReceipt(
-			td.MessageProducer.CreatePaymentChannelActor(sender, receiver, chain.Value(toSend), chain.Nonce(0)),
-			types.MessageReceipt{ExitCode: exitcode.Ok, ReturnValue: paychAddr.Bytes(), GasUsed: big_spec.Zero()},
+			td.MessageProducer.CreatePaymentChannelActor(receiver, sender, chain.Value(toSend), chain.Nonce(0)),
+			types.MessageReceipt{ExitCode: exitcode.Ok, ReturnValue: chain.MustSerialize(&initRet), GasUsed: big_spec.Zero()},
 		)
 		td.AssertBalance(paychAddr, toSend)
 
 		td.ApplyMessageExpectReceipt(
-			td.MessageProducer.PaychCollect(paychAddr, receiver, adt_spec.EmptyValue{}, chain.Nonce(1)),
+			td.MessageProducer.PaychUpdateChannelState(paychAddr, sender, paych_spec.UpdateChannelStateParams{
+				Sv: paych_spec.SignedVoucher{
+					TimeLockMin: abi_spec.ChainEpoch(1),
+					TimeLockMax: 0, // TimeLockMax set to 0 means no timeout
+					Lane:        1,
+					Nonce:       1,
+					Amount:      toSend, // the amount that can be redeemed by receiver
+					Signature: &crypto_spec.Signature{
+						Type: crypto_spec.SigTypeBLS,
+						Data: []byte("signature goes here"),
+					},
+				},
+			}, chain.Nonce(1), chain.Value(big_spec.Zero())),
 			types.MessageReceipt{ExitCode: exitcode.Ok, ReturnValue: drivers.EmptyReturnValue, GasUsed: big_spec.Zero()},
 		)
 
-		td.AssertBalance(receiver, toSend)
+		// settle the payment channel so it may be collected
+		td.ApplyMessageExpectReceipt(
+			td.MessageProducer.PaychSettle(paychAddr, receiver, adt_spec.EmptyValue{}, chain.Value(big_spec.Zero()), chain.Nonce(0)),
+			types.MessageReceipt{ExitCode: exitcode.Ok, ReturnValue: drivers.EmptyReturnValue, GasUsed: big_spec.Zero()},
+		)
+
+		// advance the epoch so the funds may be redeemed.
+		td.ExeCtx.Epoch++
+
+		td.ApplyMessageExpectReceipt(
+			td.MessageProducer.PaychCollect(paychAddr, receiver, adt_spec.EmptyValue{}, chain.Nonce(1), chain.Value(big_spec.Zero())),
+			types.MessageReceipt{ExitCode: exitcode.Ok, ReturnValue: drivers.EmptyReturnValue, GasUsed: big_spec.Zero()},
+		)
+
+		td.AssertBalance(receiver, big_spec.Add(toSend, initialBal))
 		td.AssertBalance(paychAddr, big_spec.Zero())
 		var pcState paych_spec.State
 		td.GetActorState(paychAddr, &pcState)
