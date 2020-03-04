0540a32c68da9c33b0c7756e97999ab65df26d4f
Whyrusleeping <why@ipfs.io>
2020-03-03 16:22:12 -0800

    Fix up ID allocations and drop unnecessary bytes (#97)

--- drivers/constants.go
+++ drivers/constants.go
@@ -2,20 +2,12 @@ package drivers
 
 import (
 	big_spec "github.com/filecoin-project/specs-actors/actors/abi/big"
-	adt_spec "github.com/filecoin-project/specs-actors/actors/util/adt"
-
-	"github.com/filecoin-project/chain-validation/chain"
 )
 
 const (
 	totalFilecoin     = 2_000_000_000
 	filecoinPrecision = 1_000_000_000_000_000_000
 )
-
-func init() {
-	EmptyReturnValue = chain.MustSerialize(adt_spec.EmptyValue{})
-
-}
 
 var (
 	TotalNetworkBalance = big_spec.Mul(big_spec.NewInt(totalFilecoin), big_spec.NewInt(filecoinPrecision))
