c2a65df45b244533c73e2a9b7f04555dcf6a1a06
frrist <forrest@protocol.ai>
2020-03-09 16:26:48 -0700

    add tests for invalid sender address:
    - send from address not in state tree
    - send from address that is undef
    - send from address with unknown protocol via staged message in multisig
    actor

--- suites/tipset/internal_message_failure.go
+++ suites/tipset/internal_message_failure.go
@@ -7,9 +7,12 @@ import (
 	"github.com/filecoin-project/go-address"
 	abi_spec "github.com/filecoin-project/specs-actors/actors/abi"
 	big_spec "github.com/filecoin-project/specs-actors/actors/abi/big"
+	builtin_spec "github.com/filecoin-project/specs-actors/actors/builtin"
 	init_spec "github.com/filecoin-project/specs-actors/actors/builtin/init"
+
 	multisig_spec "github.com/filecoin-project/specs-actors/actors/builtin/multisig"
 	"github.com/filecoin-project/specs-actors/actors/runtime/exitcode"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/chain-validation/chain"
@@ -34,7 +37,6 @@ func TestInternalMessageApplicationFailure(t *testing.T, factory state.Factories
 		nobody := utils.NewSECP256K1Addr(t, "pubbub")
 
 		multisigAddr := utils.NewIDAddr(t, utils.IdFromAddress(aliceId)+1)
-		createRet := td.ComputeInitActorExecReturn(aliceId, 1, multisigAddr)
 
 		createMsMsg := td.MessageProducer.CreateMultisigActor(aliceId, []address.Address{aliceId}, 10, 1,
 			chain.Nonce(0), chain.Value(big_spec.Zero()))
@@ -60,6 +62,7 @@ func TestInternalMessageApplicationFailure(t *testing.T, factory state.Factories
 		createMsRet := receipts[0]
 		proposeMsRet := receipts[1]
 
+		createRet := td.ComputeInitActorExecReturn(aliceId, 0, multisigAddr)
 		td.AssertReceipt(createMsRet, types.MessageReceipt{ExitCode: exitcode.Ok, ReturnValue: chain.MustSerialize(&createRet), GasUsed: abi_spec.NewTokenAmount(1282)})
 		td.AssertReceipt(proposeMsRet, types.MessageReceipt{ExitCode: exitcode.Ok, ReturnValue: drivers.EmptyReturnValue, GasUsed: abi_spec.NewTokenAmount(1235)})
 
@@ -70,5 +73,51 @@ func TestInternalMessageApplicationFailure(t *testing.T, factory state.Factories
 		_, err = td.State().Actor(nobody)
 		require.Error(t, err)
 		require.Contains(t, err.Error(), init_spec.ErrAddressNotFound.Error())
+	})
+
+	t.Run("multisig internal message send fails when receiver is invalid address protocol", func(t *testing.T) {
+		td := builder.Build(t)
+
+		// create the multisig actor, set number of approvals to 1 so propose goes through on first send.
+		_, aliceId := td.NewAccountActor(drivers.SECP, abi_spec.NewTokenAmount(2_000_000_000_000))
+		multisigAddr := utils.NewIDAddr(t, utils.IdFromAddress(aliceId)+1)
+		createMsMsg := td.MessageProducer.CreateMultisigActor(aliceId, []address.Address{aliceId}, 10, 1,
+			chain.Nonce(0), chain.Value(big_spec.Zero()))
+
+		// this address will be changed to protocol Unknown after its been serialized into bytes for a parameter.
+		nobody := utils.NewSECP256K1Addr(t, "pubbbub")
+		params := multisig_spec.ProposeParams{
+			To:     nobody,
+			Value:  big_spec.Zero(),
+			Method: 0,
+			Params: nil,
+		}
+		ser := chain.MustSerialize(&params)
+		ser[2] = address.Unknown // the 3rd byte in the slice is the address protocol identifier, set to invalid protocol
+		proposeMsMsg := td.MessageProducer.Build(multisigAddr, aliceId, builtin_spec.MethodsMultisig.Propose, ser, chain.Nonce(1), chain.Value(big_spec.Zero()))
+
+		// Create the multisig actor and propose the send
+		blkMsgs := chain.NewTipSetMessageBuilder().
+			WithMiner(td.ExeCtx.Miner).
+			WithSECPMessage(signMessage(createMsMsg, td.Wallet())).
+			WithSECPMessage(signMessage(proposeMsMsg, td.Wallet())).
+			Build()
+
+		receipts, err := td.Validator.ApplyTipSetMessages(td.ExeCtx, td.State(), []types.BlockMessagesInfo{blkMsgs}, td.Randomness())
+		require.NoError(t, err)
+		require.Len(t, receipts, 2)
+
+		createMsRet := receipts[0]
+		proposeMsRet := receipts[1]
+
+		createRet := td.ComputeInitActorExecReturn(aliceId, 0, multisigAddr)
+		td.AssertReceipt(createMsRet, types.MessageReceipt{ExitCode: exitcode.Ok, ReturnValue: chain.MustSerialize(&createRet), GasUsed: abi_spec.NewTokenAmount(1282)})
+		td.AssertReceipt(proposeMsRet, types.MessageReceipt{ExitCode: exitcode.SysErrInvalidParameters, ReturnValue: drivers.EmptyReturnValue, GasUsed: abi_spec.NewTokenAmount(1235)})
+
+		// the multisig txnid should increment and the 0th transaction should have been removed
+		var msa multisig_spec.State
+		td.GetActorState(multisigAddr, &msa)
+		td.AssertMultisigContainsTransaction(multisigAddr, 0, false)
+		assert.Equal(t, multisig_spec.TxnID(1), msa.NextTxnID)
 	})
 }
