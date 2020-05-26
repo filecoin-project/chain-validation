c56c1882dc99165ae4c6a285517b51785bedae2f
Frrist <forrest@protocol.ai>
2020-05-26 10:18:00 -0700

    add syscalls to testdriver and make funcs swapable (#208)

--- drivers/syscall.go
+++ drivers/syscall.go
@@ -12,22 +12,15 @@ import (
 	"github.com/minio/blake2b-simd"
 )
 
-type ChainValidationSyscalls struct {
-}
-
-func NewChainValidationSyscalls() *ChainValidationSyscalls {
-	return &ChainValidationSyscalls{}
-}
-
-func (c ChainValidationSyscalls) VerifySignature(signature crypto.Signature, signer address.Address, plaintext []byte) error {
+var fakeVerifySignatureFunc = func(signature crypto.Signature, signer address.Address, plaintext []byte) error {
 	return nil
 }
 
-func (c ChainValidationSyscalls) HashBlake2b(data []byte) [32]byte {
+var defaultHashBlake2bFunc = func(data []byte) [32]byte {
 	return blake2b.Sum256(data)
 }
 
-func (c ChainValidationSyscalls) ComputeUnsealedSectorCID(proof abi.RegisteredProof, pieces []abi.PieceInfo) (cid.Cid, error) {
+var fakeComputerUnsealedSectorCIDFunc = func(proof abi.RegisteredProof, pieces []abi.PieceInfo) (cid.Cid, error) {
 	// Fake CID computation by hashing the piece info (rather than the real computation over piece commitments).
 	buf := bytes.Buffer{}
 	for _, p := range pieces {
@@ -40,14 +33,60 @@ func (c ChainValidationSyscalls) ComputeUnsealedSectorCID(proof abi.RegisteredPr
 	return commcid.DataCommitmentV1ToCID(token[:]), nil
 }
 
-func (c ChainValidationSyscalls) VerifySeal(info abi.SealVerifyInfo) error {
+var fakeVerifySealFunc = func(info abi.SealVerifyInfo) error {
 	return nil
 }
 
-func (c ChainValidationSyscalls) VerifyPoSt(info abi.WindowPoStVerifyInfo) error {
+var fakeVerifyPoStFunc = func(info abi.WindowPoStVerifyInfo) error {
 	return nil
 }
 
-func (c ChainValidationSyscalls) VerifyConsensusFault(h1, h2, extra []byte) (*runtime.ConsensusFault, error) {
+var panicingVerifyConsensusFaultFunc = func(h1, h2, extra []byte) (*runtime.ConsensusFault, error) {
 	panic("implement me")
+}
+
+type ChainValidationSysCalls struct {
+	VerifySigFunc                func(signature crypto.Signature, signer address.Address, plaintext []byte) error
+	HashBlake2bFunc              func(data []byte) [32]byte
+	ComputeUnSealedSectorCIDFunc func(proof abi.RegisteredProof, pieces []abi.PieceInfo) (cid.Cid, error)
+	VerifySealFunc               func(info abi.SealVerifyInfo) error
+	VerifyPoStFunc               func(info abi.WindowPoStVerifyInfo) error
+	VerifyConsensusFaultFunc     func(h1, h2, extra []byte) (*runtime.ConsensusFault, error)
+}
+
+func NewChainValidationSysCalls() *ChainValidationSysCalls {
+	return &ChainValidationSysCalls{
+		HashBlake2bFunc: defaultHashBlake2bFunc,
+
+		VerifySigFunc:                fakeVerifySignatureFunc,
+		ComputeUnSealedSectorCIDFunc: fakeComputerUnsealedSectorCIDFunc,
+		VerifySealFunc:               fakeVerifySealFunc,
+		VerifyPoStFunc:               fakeVerifyPoStFunc,
+
+		VerifyConsensusFaultFunc: panicingVerifyConsensusFaultFunc,
+	}
+}
+
+func (c ChainValidationSysCalls) VerifySignature(signature crypto.Signature, signer address.Address, plaintext []byte) error {
+	return c.VerifySigFunc(signature, signer, plaintext)
+}
+
+func (c ChainValidationSysCalls) HashBlake2b(data []byte) [32]byte {
+	return c.HashBlake2bFunc(data)
+}
+
+func (c ChainValidationSysCalls) ComputeUnsealedSectorCID(proof abi.RegisteredProof, pieces []abi.PieceInfo) (cid.Cid, error) {
+	return c.ComputeUnSealedSectorCIDFunc(proof, pieces)
+}
+
+func (c ChainValidationSysCalls) VerifySeal(info abi.SealVerifyInfo) error {
+	return c.VerifySealFunc(info)
+}
+
+func (c ChainValidationSysCalls) VerifyPoSt(info abi.WindowPoStVerifyInfo) error {
+	return c.VerifyPoStFunc(info)
+}
+
+func (c ChainValidationSysCalls) VerifyConsensusFault(h1, h2, extra []byte) (*runtime.ConsensusFault, error) {
+	return c.VerifyConsensusFaultFunc(h1, h2, extra)
 }
